ea3c6055223db8d534c2735fa1a0c17bb3b268c5
trofimchuk <trofimchuk@itstep.academy>
2021-06-23 19:18:42 +0300

    Init

---
+++ Shop18/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop18.Data;
+using Shop18.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop18.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Category;
+            return View(categoryList);
+        }
+    }
+}
