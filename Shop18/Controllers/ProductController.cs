198620f6d2bcebedd3b87ac1e837aee2e1881993
trofimchuk <trofimchuk@itstep.academy>
2021-06-28 19:27:15 +0300

    Products

--- Shop18/Controllers/ProductController.cs
+++ Shop18/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Shop18.Data;
@@ -6,6 +7,7 @@ using Shop18.Models;
 using Shop18.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,9 +16,12 @@ namespace Shop18.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public ProductController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvirinment;
+
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvirinment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -54,15 +59,60 @@ namespace Shop18.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Product product)
+        public IActionResult Upsert(ProductVM productVM)
         {
             if (ModelState.IsValid)
             {
-                _db.Product.Add(product);
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvirinment.WebRootPath;
+                string upload = webRootPath + ENV.ImagePath;
+
+                string fileName = Guid.NewGuid().ToString();
+                string extensions = Path.GetExtension(files[0].FileName);
+
+                if(productVM.Product.Id == 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extensions), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    productVM.Product.Image = fileName + extensions;
+                    _db.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    var formObject = _db.Product.AsNoTracking().FirstOrDefault(u => u.Id == productVM.Product.Id);
+                    if(files.Count > 0)
+                    {
+                        var oldFile = Path.Combine(upload, formObject.Image);
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extensions), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+                        productVM.Product.Image = fileName + extensions;
+                    }
+                    else
+                    {
+                        productVM.Product.Image = formObject.Image;
+                    }
+                    _db.Product.Update(productVM.Product);
+                }
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(product);
+            productVM.CategorySelectList = _db.Category.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }); ;
+            return View(productVM);
         }
     }
 }
---
+++ Shop18/ENV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop18
+{
+    public class ENV
+    {
+        public const string ImagePath = @"\images\products\";
+    }
+}
