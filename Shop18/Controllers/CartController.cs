dcd628901ba6f47d9916ae3f02fa34ddc70af0c9
trofimchuk <trofimchuk@itstep.academy>
2021-07-16 18:52:28 +0300

    Order

--- Shop18/Controllers/CartController.cs
+++ Shop18/Controllers/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Shop18.Data;
 using Shop18.Models;
@@ -7,8 +9,10 @@ using Shop18.Models.ViewModels;
 using Shop18.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Shop18.Controllers
@@ -17,9 +21,14 @@ namespace Shop18.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public CartController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IEmailSender _emailSender;
+
+        public CartController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment, IEmailSender emailSender)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
+            _emailSender = emailSender;
         }
 
         public IActionResult Index()
@@ -36,6 +45,14 @@ namespace Shop18.Controllers
             IEnumerable<Product> productList = _db.Product.Where(u => prductinCart.Contains(u.Id));
 
             return View(productList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Order));
         }
 
         public IActionResult Remove(int id)
@@ -81,11 +98,36 @@ namespace Shop18.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [ActionName("Order")]
-        public IActionResult OrderPost(ProductUserVM productUserVM)
+        public async Task<IActionResult> OrderPost(ProductUserVM productUserVM)
         {
-            // Send email
+            var PathToTemplate = _webHostEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()
+                + "templates" + Path.DirectorySeparatorChar.ToString()
+                + "OrderConfirmation.html";
 
-            return RedirectToAction(nameof(OrderConfirmation));
+            var subject = "New order";
+            string HtmlBody = "";
+
+            using (StreamReader sr = System.IO.File.OpenText(PathToTemplate))
+            {
+                HtmlBody = sr.ReadToEnd();
+            }
+
+
+            StringBuilder productListSB = new StringBuilder();
+            foreach(var item in productUserVM.ProductList)
+            {
+                productListSB.Append($"Name: {item.Name}\nPrice: {item.Price}");
+            }
+
+            string messageBody = string.Format(
+                HtmlBody,
+                productUserVM.AppUser.FullName,
+                productUserVM.AppUser.Email,
+                productListSB.ToString()
+                );
+
+            await _emailSender.SendEmailAsync(ENV.AdminEmail, subject, messageBody);
+                return RedirectToAction(nameof(OrderConfirmation));
         }
 
         public IActionResult OrderConfirmation()
--- Shop18/ENV.cs
+++ Shop18/ENV.cs
@@ -10,7 +10,9 @@ namespace Shop18
         public const string ImagePath = @"\images\products\";
         public const string SessionCart = "EcBvhuxyH*8AQk_*Mz3LGKcV&fzg778*";
 
-        public static string AdminRole = "Admin";
-        public static string CustomerRole = "Customer";
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public const string AdminEmail = "trofimchuk.an@gmail.com";
     }
 }
